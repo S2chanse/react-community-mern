635a9d32cebd2f48f8c2089e0a40f3dea0ae53c9
css <seop7379@daum.net>
2022-11-07 15:36:10 +0900

    build script

--- App/client/src/Component/Post/Main.js
+++ App/client/src/Component/Post/Main.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import List from "./List";
-import Dropdown from "react-bootstrap/Dropdown";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import List from './List';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function Main() {
   const [contentList, setContentList] = useState([]);
-  const [sort, setSort] = useState("최신순");
-  const [searchTerm, setSearchTerm] = useState("");
+  const [sort, setSort] = useState('최신순');
+  const [searchTerm, setSearchTerm] = useState('');
   const [skip, setSkip] = useState(0);
   const [loadMore, setLoadMore] = useState(true);
 
   useEffect(() => {
-    SearchHandler();
+    searchHandler();
   }, []);
-
-  const SearchHandler = () => {
+  const getListHandler = () => {
     axios
-      .post("/api/post/list", { sort, searchTerm, skip })
+      .post('/api/post/list', { sort, searchTerm, skip })
       .then((res) => {
         if (res.data.success) {
           let postList = res.data.postList;
@@ -32,17 +31,29 @@ export default function Main() {
       })
       .catch((err) => console.log(err));
   };
+
+  const searchHandler = () => {
+    axios
+      .post('/api/post/list', { sort, searchTerm, skip })
+      .then((res) => {
+        if (res.data.success) {
+          let postList = res.data.postList;
+          setContentList([...postList]);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div>
-      <InputGroup className="mb-3">
+      <InputGroup className='mb-3'>
         <Dropdown>
-          <Dropdown.Toggle variant="outline-secondary">{sort}</Dropdown.Toggle>
+          <Dropdown.Toggle variant='outline-secondary'>{sort}</Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => setSort("최신순")}>
+            <Dropdown.Item onClick={() => setSort('최신순')}>
               최신순
             </Dropdown.Item>
-            <Dropdown.Item onClick={() => setSort("인기순")}>
+            <Dropdown.Item onClick={() => setSort('인기순')}>
               인기순
             </Dropdown.Item>
           </Dropdown.Menu>
@@ -50,22 +61,22 @@ export default function Main() {
         <Form.Control
           placeholder="Recipient's username"
           aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
+          aria-describedby='basic-addon2'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.currentTarget.value)}
         />
-        <Button variant="outline-secondary" onClick={() => SearchHandler()}>
+        <Button variant='outline-secondary' onClick={() => searchHandler()}>
           Button
         </Button>
       </InputGroup>
       <List contentList={contentList} />
-      <div className="d-grid gap-2">
+      <div className='d-grid gap-2'>
         {loadMore ? (
           <>
             <Button
-              variant="secondary"
-              size="lg"
-              onClick={() => SearchHandler()}
+              variant='secondary'
+              size='lg'
+              onClick={() => getListHandler()}
             >
               더 불러오기
             </Button>
